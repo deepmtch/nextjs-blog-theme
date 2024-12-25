636794d69e5132fb31492c96d01cfb7b88d929ed
AI Rabbit <148769813+deepmtch@users.noreply.github.com>
2024-12-25 12:20:10 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,26 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
           <Main />
           <NextScript />
+          <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
+          <script type="text/javascript" dangerouslySetInnerHTML={{
+            __html: `
+              algoliasearchNetlify({
+                appId: 'F4TNI6LSR6',
+                apiKey: '99a85f9f246db0f1bc851eb62c1d487b',
+                siteId: '2f23ad21-13c9-4bd5-9615-63f9f6c52196',
+                branch: 'main',
+                selector: 'div#search',
+              });
+            `
+          }} />
         </body>
       </Html>
     );
