60f99b5bfc3a31d4c22349c58ad1353342f5cc2d
AI Rabbit <148769813+deepmtch@users.noreply.github.com>
2024-12-25 12:40:18 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -14,21 +13,6 @@ class MyDocument extends Document {
         >
           <Main />
           <NextScript />
-          <Script
-            src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"
-            strategy="afterInteractive"
-          />
-          <Script id="algolia-init" strategy="afterInteractive">
-            {`
-              algoliasearchNetlify({
-                appId: 'F4TNI6LSR6',
-                apiKey: '99a85f9f246db0f1bc851eb62c1d487b',
-                siteId: '2f23ad21-13c9-4bd5-9615-63f9f6c52196',
-                branch: 'main',
-                selector: 'div#search',
-              });
-            `}
-          </Script>
         </body>
       </Html>
     );
